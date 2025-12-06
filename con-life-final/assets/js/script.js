9ac586d9d0fbeecd56f23f26e95f1e1412ba215d
Christine M Jones <cmjones10@ualr.edu>
2025-12-06 18:53:36 +0000

    table removal

--- con-life-final/assets/js/script.js
+++ con-life-final/assets/js/script.js
@@ -10,6 +10,30 @@ $(document).ready(function() {
       autoplay: true,
       autoplaySpeed: 2000
     });
+});
 
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tabbuttons;
 
+  // Get all elements with class="tab-content" and hide them
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tab-button" and remove the "active" class
+  tabbuttons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabbuttons.length; i++) {
+    tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Set the first tab as active by default on page load
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.tab-button').click();
 });
